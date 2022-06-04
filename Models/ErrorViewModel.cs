faa4e7247d54f97133cc9cb74f27063271d50d5e
ajmalrahiman <ajmalrahiman@outlook.com>
2022-06-04 05:37:45 +0530

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GithubActions2DockerHub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
